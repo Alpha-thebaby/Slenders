ea318a01f8222ef6e61eb2f549b8de79d71a9297
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-02-03 22:10:32 -0500

    Removed all mob spawning and added slender spawning
    Mobs will still spawn through game mechanics, structures, and features

--- fabric-example-mod-1.19/src/main/java/kelvin/slendermod/common/entities/EntityRegistry.java
+++ fabric-example-mod-1.19/src/main/java/kelvin/slendermod/common/entities/EntityRegistry.java
@@ -1,24 +1,30 @@
 package kelvin.slendermod.common.entities;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.tag.BiomeTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.Heightmap;
 
 public class EntityRegistry {
     public static EntityType<EntitySlenderman> SLENDERMAN = Registry.register(
             Registry.ENTITY_TYPE,
             new Identifier("slendermod", "slenderman"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, EntitySlenderman::new).dimensions(EntityDimensions.fixed(0.75f, 2.9f)).build()
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, EntitySlenderman::new).dimensions(EntityDimensions.fixed(0.75f, 2.9f)).build()
     );
 
     public static EntityType<EntitySmallSlender> SMALL_SLENDER = Registry.register(
             Registry.ENTITY_TYPE,
             new Identifier("slendermod", "small_slender"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, EntitySmallSlender::new).dimensions(EntityDimensions.fixed(0.75f, 1.5f)).build()
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, EntitySmallSlender::new).dimensions(EntityDimensions.fixed(0.75f, 1.5f)).build()
     );
 
     public static EntityType<EntitySlenderBoss> SLENDER_BOSS = Registry.register(
@@ -32,5 +38,15 @@ public class EntityRegistry {
         FabricDefaultAttributeRegistry.register(SLENDERMAN, EntitySlenderman.createMobAttributes());
         FabricDefaultAttributeRegistry.register(SLENDER_BOSS, EntitySlenderBoss.createAttributes());
         FabricDefaultAttributeRegistry.register(SMALL_SLENDER, EntitySmallSlender.createMobAttributes());
+
+        spawnSlender(SLENDERMAN);
+        spawnSlender(SMALL_SLENDER);
+    }
+
+    private static <T extends MobEntity> void spawnSlender(EntityType<T> slender) {
+        SpawnRestriction.register(slender, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, (type, world, spawnReason, pos, random) ->
+                world.getDifficulty() != Difficulty.PEACEFUL && world.getBlockState(pos.down()).allowsSpawning(world, pos, type));
+
+        BiomeModifications.addSpawn(context -> context.hasTag(BiomeTags.IS_OVERWORLD) || context.hasTag(BiomeTags.IS_NETHER) || context.hasTag(BiomeTags.IS_END), SpawnGroup.CREATURE, slender, 25, 1, 3);
     }
 }
