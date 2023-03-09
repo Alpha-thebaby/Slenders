0d3d81d2324697ba72c6d0c1f219649ad4e35313
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-03-08 19:45:31 -0500

    Fixed bones

--- src/main/java/kelvin/slendermod/block/BonesBlock.java
+++ src/main/java/kelvin/slendermod/block/BonesBlock.java
@@ -2,13 +2,17 @@ package kelvin.slendermod.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 
-public class BonesBlock extends CustomFacingBlock {
+public class BonesBlock extends HorizontalFacingBlock {
 
     public static VoxelShape NORTH_SHAPE = Block.createCuboidShape(1.0, 0.0, 0.0, 14.0, 6.0, 16.0);
     public static VoxelShape SOUTH_SHAPE = Block.createCuboidShape(2.0, 0.0, 0.0, 15.0, 6.0, 16.0);
@@ -28,5 +32,13 @@ public class BonesBlock extends CustomFacingBlock {
             case EAST -> EAST_SHAPE;
             default -> NORTH_SHAPE;
         };
+    }
+
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
     }
 }
